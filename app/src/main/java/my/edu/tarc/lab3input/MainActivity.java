764b951f328b2c7d831bc031762f8056d516e374
khorch-pa16@student.tarc.edu.my <Kch981003>
2018-12-03 17:57:55 +0800

    commit

--- app/src/main/java/my/edu/tarc/lab3input/MainActivity.java
+++ app/src/main/java/my/edu/tarc/lab3input/MainActivity.java
@@ -2,12 +2,36 @@ package my.edu.tarc.lab3input;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+   private Spinner spinnerAge;
+   private RadioGroup radioGroupGender;
+   private RadioButton radioButtonMale,radioButtonFemale;
+   private CheckBox checkBoxSmoker;
+   private TextView tetxViewPremium;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerAge=findViewById(R.id.spinnerAge);
+        radioGroupGender=findViewById(R.id.radioGroupGender);
+        radioButtonMale=findViewById(R.id.radioButtonMale);
+        radioButtonFemale=findViewById(R.id.radioButtonFemale);
+        checkBoxSmoker=findViewById(R.id.checkBoxSmoker);
+        tetxViewPremium=findViewById(R.id.textViewPremium);
+        ArrayAdapter<CharSequence> adapter=
+                ArrayAdapter.createFromResource(this,R.array.age_group,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAge.setAdapter(adapter);
     }
+
+
 }
